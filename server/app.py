a01723f8ab02571be0fdd3caaba1a859f4f700ed
BuildTools <fhebert-perkins16@bayschoolsf.org>
2015-04-19 11:11:55 -0700

    verify url herpderp

--- server/app.py
+++ server/app.py
@@ -4,32 +4,37 @@ from crossdomain import crossdomain
 import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.secret_key = os.urandom.encode("base_64")
+app.secret_key = os.urandom.encode('base_64')
 db = SQLAlchemy(app)
 
-@app.route("/admin")
+@app.route('/admin')
 def admin():
-    if not session.get("logged_in"):
-        redirect(url_for("login"))
-    return "admin panel"
+    if not session.get('logged_in'):
+        redirect(url_for('login'))
+    return 'admin panel'
 
-@app.route("/login")
+@app.route('/login')
 def login():
-    if session.get("logged_in"):
-        redirect(url_for("admin"))
-    return "login page"
+    if session.get('logged_in'):
+        redirect(url_for('admin'))
+    return 'login page'
 
 
-@app.route("/verify", methods=["POST"])
-@crossdomain(origin="*")
+@app.route('/verify', methods=['POST'])
+@crossdomain(origin='*')
 def verify():
-    print request.form
-    return "herpderp"
+    cid = request.form['cid']
+    Clients(sid=cid,
+            browser=request.form['bname'],
+            browserVersion = request.form['version'],
+            language = request.form['language'],
+            platform = request.form['platform']).save()
+    return 'logged'
 
-@app.route("/data", methods=["POST"])
-@crossdomain(origin="*")
+@app.route('/data', methods=['POST'])
+@crossdomain(origin='*')
 def data():
-    print request.form["cid"]
-    return "derp"
-if __name__ == "__main__":
+    print request.form['cid']
+    return 'derp'
+if __name__ == '__main__':
     app.run(debug=True)
